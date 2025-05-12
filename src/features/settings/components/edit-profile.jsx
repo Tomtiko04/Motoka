423c923e2f8935b4a37a55009240a32464a577c0
Sulaimon Taofeek <sulaimontaofeekgithub100@gmail.com>
2025-05-12 02:05:12 +0100

    fix changes

--- src/features/settings/components/edit-profile.jsx
+++ src/features/settings/components/edit-profile.jsx
@@ -70,7 +70,7 @@ export default function EditProfile({ onNavigate }) {
         
         resetProfileState()
         
-        
+      
 
         setTimeout(() => {
           onNavigate("profile")
