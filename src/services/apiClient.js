940cd5796dd7fbfa891f85dabbe4674016c1bbe6
Sulaimon Taofeek <sulaimontaofeekgithub100@gmail.com>
2025-07-21 16:03:06 +0100

    api update

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -6,7 +6,7 @@ import { authStorage } from "../utils/authStorage"
 const requestCache = new Map()
 
 const api = axios.create({
-  baseURL: "https://backend.motoka.com.ng/api",
+  baseURL: "https://api.motoka.ng/api",
 })
 
 // Add security headers to all requests
