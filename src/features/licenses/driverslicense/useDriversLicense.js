5acea24e5fdd1d6d06c1990db96bdea63ede3e34
Ogunneye Oyinkansola Munirat <ogunneyeoyinkansola5@gmail.com>
2026-03-09 17:32:00 +0100

    Staging (#205)
    * feat: update drivers license flows and international pricing
    
    * feat: update drivers license flows and international pricing
    
    * fix: add missing useDriversLicensePaymentOptions and useCreateDriverLicense hooks
    
    ---------
    
    Co-authored-by: oladimeji402 <olamoney665@gmail.com>
    Co-authored-by: Adewara OluwaPelumi <84779837+Dave1604@users.noreply.github.com>

--- src/features/licenses/driverslicense/useDriversLicense.js
+++ src/features/licenses/driverslicense/useDriversLicense.js
@@ -1,11 +1,40 @@
-import { useQuery } from "@tanstack/react-query";
-import { getDriverLicensePrices } from "../../../services/apiDriversLicense";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  createDriversLicense,
+  getDriversLicensePaymentOptions,
+} from "../../../services/apiDriversLicense";
 
 export function useDriverLicensePrices() {
   const { data: prices, isLoading, error } = useQuery({
     queryKey: ["driver-license-prices"],
-    queryFn: getDriverLicensePrices,
+    queryFn: getDriversLicensePaymentOptions,
   });
 
   return { prices, isLoading, error };
+}
+
+export function useDriversLicensePaymentOptions() {
+  const {
+    data: isPaymentOptions,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["driver-license-prices"],
+    queryFn: getDriversLicensePaymentOptions,
+  });
+
+  return { isPaymentOptions, isLoading, error };
+}
+
+export function useCreateDriverLicense() {
+  const queryClient = useQueryClient();
+
+  const { mutate: createLicense, isPending: isCreating } = useMutation({
+    mutationFn: createDriversLicense,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["driver-license-application"] });
+    },
+  });
+
+  return { createLicense, isCreating };
 }
